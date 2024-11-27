42bcc8214da43d511b9c3e847270ca89efb8f49c
nishh1810 <101450921@georgebrown.ca>
2024-11-27 18:56:42 -0500

    Cors added

--- app.js
+++ app.js
@@ -3,18 +3,20 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // Connect to MongoDB
 // mongoose.connect('mongodb://localhost:27017/admin');
 mongoose.connect('mongodb+srv://nishitasachdev95:Admin%401121@cluster0.ft341.mongodb.net/');
 
+app.use(cors());
 app.use(bodyParser.json());
 // Define API endpoints
 app.use('/api/v1/user', require('./routes/users'));
 app.use('/api/v1/emp', require('./routes/employees'));
 
 // Start the server
-const port = 8087 ;
+const port = 7070 ;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
